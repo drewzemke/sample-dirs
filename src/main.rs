76f36e8bc51b797737c5380975c2b3aaded61dc1
Drew Zemke <drew.zemke@gmail.com>
2023-07-04 12:41:10 -0700

    feat(errors): error handling for initial directory read

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,7 @@
 
 use clap::Parser;
 use reservoir_sampling::unweighted::l as sample;
-use std::{
-    error::Error,
-    fs::{self, read_dir},
-    path::PathBuf,
-};
+use std::{error::Error, fs, io, path::PathBuf};
 
 /// Randomly samples the top-level subdirectories of a given directory, and places the results in an output directory.
 #[derive(Parser, Debug)]
@@ -34,17 +30,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     // gather list of subdirectories
-    let subdirectories: Vec<PathBuf> = read_dir(args.in_dir.clone())?
-        .map(|entry| entry.unwrap().path())
-        .filter(|path| path.is_dir())
-        .collect();
+    let subdirectories = read_subdirs(&args.in_dir)
+        .map_err(|e| format!("Could not read directory '{}': {e}", args.in_dir.display()))?;
 
     // get the list of files to copy
     let files: Vec<PathBuf> = subdirectories
         .iter()
         .flat_map(|dir| {
             // read files in subdirec
-            let files = read_dir(dir)
+            let files = fs::read_dir(dir)
                 .unwrap()
                 .map(|entry| entry.unwrap().path())
                 .filter(|path| path.is_file());
@@ -57,20 +51,30 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect();
 
     // create the output directories
-    subdirectories.iter().for_each(|in_subdir| {
+    for in_subdir in &subdirectories {
         let out_subdir = args
             .out_dir
             .join(in_subdir.strip_prefix(&args.in_dir).unwrap());
         fs::create_dir_all(out_subdir).unwrap();
-    });
+    }
 
     // copy files to output directory
-    files.iter().for_each(|in_path| {
+    for in_path in &files {
         let out_path = args
             .out_dir
             .join(in_path.strip_prefix(&args.in_dir).unwrap());
         let _ = fs::copy(in_path, out_path).unwrap();
-    });
+    }
 
     Ok(())
+}
+
+fn read_subdirs(dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
+    let dirs = fs::read_dir(dir)?
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .map(|entry| entry.path())
+        .filter(|path| path.is_dir())
+        .collect();
+    Ok(dirs)
 }
